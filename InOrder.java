70b0bb494fab83cb38a4256395f68adacf15c687
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2023-12-31 18:20:10 +0200

    InOrder.java

--- InOrder.java
+++ InOrder.java
@@ -1,14 +1,14 @@
 public class InOrder {
 	public static void main (String[] args) {
-	int  nam1 = (int)(math.random()*10);
-	int  nam2 = (int)(math.random()*10);
+	int  nam1 = (int)(Math.random()*10);
+	int  nam2 = (int)(Math.random()*10);
 	int temp =  nam1;
 	String.out.print(nam1);
 
 	while(temp <=  nam2){
 		String.out.print(" " +  nam2);
 		tamp =  nam2;
-		 nam2 = (int)(Math.round()*10)
+		 nam2 = (int)(Math.round()*10);
 		
 	}
 

090454b5281089268c08e181afd47cd4d649bb2b
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-01 17:48:30 +0200

    DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,11 +6,11 @@ public class DamkaBoard {
 			for (int j = 0; j < nam; j++) {
 
 				if ((i + j) % 2 == 0) {
-					System.out.print("*");
+					System.out.print("* ");
 				}
 				else {
-
-					System.out.print(" * ");
+					System.out.print(" ");
+					System.out.print("* ");
 				}
 			}
 			System.out.println();

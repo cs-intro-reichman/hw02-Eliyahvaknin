2cde98f7fdf2220ddebaecbf37d45cbbe1f8b251
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-01 12:23:55 +0200

    Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -1,18 +1,27 @@
-public class Perfect {
-	public static void main (String[] args) {
-	int nam = Integer.parseInt(args[0]);
-	int sam = 0;
+public class Perfect1 {
+    public static void main (String[] args) {
+    int nam = Integer.parseInt(args[0]);
+    int sam = 0;
         for(int i = 1; i < nam ; i++){
             if (nam % i== 0){
-            	sam = i + sam ;
+                sam = i + sam ;
+
 
             }
 
         }
-        if (sam == nam) 
-        	System.out.println(nam +  " " +"is perfect number");
-        else
-            System.out.println(nam + " "+" is not a perfect number");
-        
-     }
-}
+        if (sam == nam) {
+            System.out.print(nam +  " " +"is a perfect number since " + nam +" = 1"  );
+                    for(int i = 2; i <= (nam/2) ; i++){
+                        if (nam % i== 0){
+                            System.out.print(" + " + i );
+            
+                        }
+            
+                    }
+                }
+                else
+            System.out.println(nam + " is not a perfect number");
+            }
+            
+        }

e679fc9dc74d8505680d2b4833538c0b21ab6932
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-01 23:17:49 +0200

    OneOfEachStats1.java

--- OneOfEachStats1.java
+++ OneOfEachStats1.java
@@ -1,11 +1,55 @@
-/**
- *  Computes some statistics about families in which the parents decide 
- *  to have children until they have at least one child of each gender.
- *  The program expects to get one command-line argument: an int value
- * 	that determines how many families to simulate.
- */
+
 public class OneOfEachStats1 {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
+
+        int T = Integer.parseInt(args[0]);
+
+        int totalkids = 0;
+        int[] familySize = new int[5]; 
+        int maxSizeCount = 0;
+        int mostCommonFamilySize = 0;
+
+        for (int i = 0; i < T; i++) {
+            int numberOfChildren = simulateOneFamily();
+            totalkids += numberOfChildren;
+
+            if (numberOfChildren >= 4) {
+                familySize[4]++;
+            } else {
+                familySize[numberOfChildren]++;
+            }
+
+
+            if (familySize[numberOfChildren] > maxSizeCount) {
+                maxSizeCount = familySize[numberOfChildren];
+                mostCommonFamilySize = numberOfChildren;
+            }
+        }
+
+
+        double averageChildren = (double) totalkids / T;
+        System.out.printf("Average: %.9f children to get at least one of each gender.%n", averageChildren);
+        System.out.println("Number of families with 2 children: " + familySize[2]);
+        System.out.println("Number of families with 3 children: " + familySize[3]);
+        System.out.println("Number of families with 4 or more children: " + familySize[4]);
+        System.out.println("The most common number of children is " + (mostCommonFamilySize >= 4 ? "4 or more" : mostCommonFamilySize) + ".");
+    }
+
+    private static int simulateOneFamily() {
+        boolean boyBorn = false;
+        boolean girlBorn = false;
+        int numberkids = 0;
+
+        while (!(boyBorn && girlBorn)) {
+            if (Math.random() < 0.5) {
+                boyBorn = true;
+            } else {
+                girlBorn = true;
+            }
+
+            numberkids++;
+        }
+
+        return numberkids;
+    }
 }

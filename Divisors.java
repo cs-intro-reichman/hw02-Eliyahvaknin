0d0d94e496cd510c8871bfa01f2499ac1cff6a63
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-01 14:42:10 +0200

    Reverse.java

--- Divisors.java
+++ Divisors.java
@@ -1,12 +1,14 @@
-public class Divisors {
-    public static void main (String[] args) {
-        int nam = Integer.parseInt(args[0]);
-        for(int i = 1; i <= nam ; i++){
-            if (nam % i== 0){
-                System.out.println(i);
-
-            }
-
+public class Reverse {
+    public static void main (String[] args){
+        String input = args[0];
+        for (int i = input.length() - 1; i >= 0; i--) {
+            System.out.print(input.charAt(i));
         }
+        System.out.println();
+              int middle = input.length() / 2;
+
+               char mid = input.charAt(middle);
+               
+               System.out.println("The middle character " + mid);
     }
 }
--- Reverse.java
+++ Reverse.java
@@ -4,6 +4,7 @@ public class Reverse {
         for (int i = input.length() - 1; i >= 0; i--) {
             System.out.print(input.charAt(i));
         }
+        System.out.println();
 		      int middle = input.length() / 2;
 
 		       char mid = input.charAt(middle);
@@ -11,3 +12,4 @@ public class Reverse {
 		       System.out.println("The middle character " + mid);
     }
 }
+

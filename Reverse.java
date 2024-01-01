aa1fe045cd70a0ab088a183e6cdcabf563eb28b5
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-01 15:46:01 +0200

    Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -3,7 +3,8 @@ public class Reverse {
         String input = args[0];
         System.out.println();
         for (int i = input.length() - 1; i >= 0; i--) {
-            System.out.print(input.charAt(i)+" ");
+            System.out.print(input.charAt(i));
+            System.out.print(" ");
         }
         System.out.println();
 		      int middle = (input.length()-1)/2;

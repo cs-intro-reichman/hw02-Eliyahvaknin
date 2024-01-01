38220e29136b2494fb380e0ba80458857a1275c2
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-01 16:31:47 +0200

    Reverse.java

--- Reverse.java
+++ Reverse.java
@@ -1,6 +1,6 @@
 public class Reverse {
 	public static void main (String[] args){
-        String input ="abc";// args[0];
+        String input = args[0];
         System.out.println();
         for (int i = input.length() - 1; i >= 0; i--) {
             System.out.print(input.charAt(i));

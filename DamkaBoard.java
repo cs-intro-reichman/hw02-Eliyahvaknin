26e12706b2a65151f838dc2b42aaea046d1e1db2
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-01 17:37:02 +0200

    DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,19 +1,19 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
-				int nam = Integer.parseInt(args[0]);
+		int nam = Integer.parseInt(args[0]);
 
-				for (int i = 0; i < nam; i++) {
-					for (int j = 0; j < nam; j++) {
+		for (int i = 0; i < nam; i++) {
+			for (int j = 0; j < nam; j++) {
 
-						if ((i + j) % 2 == 0) {
-							System.out.print(" "+'*'+" ");
-						}
-						else {
-							System.out.print(" ");
-							System.out.print(" "+'*'+" ");
-						}
-					}
-					System.out.println();
+				if ((i + j) % 2 == 0) {
+					System.out.print("*");
+				}
+				else {
+
+					System.out.print(" * ");
 				}
 			}
+			System.out.println();
 		}
+	}
+}

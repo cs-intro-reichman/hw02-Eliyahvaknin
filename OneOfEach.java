f60b6f19b7399772df418a9fcf9e282f6a5d9828
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-01 22:29:33 +0200

    OneOfEach.java

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,21 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
-	}
-}
+        boolean boy = true;
+        boolean girl = true;
+        int Children = 0;
+
+        while ((boy && girl)) {
+            if (Math.random() < 0.5) {
+                System.out.print("b"+" ");
+                boy = true;
+            } else {
+                System.out.print("g ");
+                girl = true;
+            }
+            Children++;
+        }
+        System.out.println();
+        System.out.println("You made it... and you now have " + Children + " children.");
+    }
+}	

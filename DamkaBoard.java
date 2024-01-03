17dda600f5aba55336087ff295a34c3caa0d02c4
Eliyahvaknin <eliyahu.vaknin@post.runi.ac.il>
2024-01-03 15:26:28 +0200

    DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,4 +1,4 @@
-public class DamkaBoard1 {
+public class DamkaBoard {
     public static void main(String[] args) {
         int x = Integer.parseInt(args[0]);
         for (int i = 0; i < x; i++) {
